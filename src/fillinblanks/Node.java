6e5d06d70c7ba3c8d284e77e7c52088fb2e8a0c4
raigomes <rai.gomes.ismart@gmail.com>
2013-12-15 00:47:04 -0200

    Falta add o código das buscas e a entrada do algoritmo!

--- src/fillinblanks/Node.java
+++ src/fillinblanks/Node.java
@@ -9,14 +9,14 @@ package fillinblanks;
  * @author rai
  */
 public class Node {
-    private char value;
-    private int cost;
+    private char name;
+    private float cost, function;
     private char[][] matrix;
     private Node son, dad;
 
     public Node() {
-        cost = 9;
-        value = '$';
+        cost = 9.0f; function = 0.0f;
+        name = '$';
         matrix = new char[4][4];
         for (int i = 0; i < matrix.length; i++) {
             matrix[0][i] = '$';
@@ -25,31 +25,58 @@ public class Node {
         son = null; dad = null;
     }
 
-    public Node(char value, int cost, char[][] matrix, Node son) {
-        this.value = value;
+    public Node(char name, int cost, char[][] matrix, Node son) {
+        this.name = name;
         this.cost = cost;
         this.matrix = matrix;
         this.son = son;
     }
         
 
-    public char getValue() {
-        return value;
+    public char getName() {
+        return name;
     }
 
     public void setValue(char value) {
-        this.value = value;
+        this.name = value;
     }
     
 
-    public int getCost() {
+    public float getCost() {
         return cost;
     }
 
-    public void setCost(int cost) {
+    public void setCost(float cost) {
         this.cost = cost;
     }
 
+    public float getFunction() {
+        return function;
+    }
+
+    public void setFunction(float function) {
+        this.function = function;
+    }
+    
+    public Node getDad() {
+        return dad;
+    }
+
+    public void setDad(Node dad) {
+        this.dad = dad;
+    }
+    
+
+    public Node getSon() {
+        return son;
+    }
+
+    public void setSon(Node son) {
+        this.son = son;
+    }
+
+    
+    
     public char[][] getMatrix() {
         return matrix;
     }
--- src/fillinblanks/Tree.java
+++ src/fillinblanks/Tree.java
@@ -5,10 +5,14 @@
 package fillinblanks;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  *
@@ -19,6 +23,7 @@ public class Tree {
     
     private final String path = "";
     private final String heuristicFileName = "";
+    private final String dicFileName = "";
 
     public Tree() {
         init = new Node();
@@ -28,14 +33,36 @@ public class Tree {
         return init;
     }
     
-    public void AEstrela() {
+    public void search(List<Node> input) {
+        AStar(input);
+        HillClimbing(input);
+        SimmulatedAnnealing(input);
+    }
+    
+    public void AStar(List<Node> input) {        
         List<Node> frontier = new LinkedList<Node>();
+        Set<String> explored = new HashSet<String>();
         Map<String, Float> heuristic = getHeuristicFromFile();
+        SortedSet<String> dictionary = getDictionaryFromFile();
         
+        frontier = sortedAdd(frontier, input, heuristic);
         
-        
+        AStar(init, frontier, explored, heuristic, dictionary);
+                
+    }
+    
+    private void AStar(Node node, List<Node> frontier, Set<String> explored, Map<String, Float> heuristic, SortedSet<String> dictionary) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    private void HillClimbing(List<Node> input) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void SimmulatedAnnealing(List<Node> input) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }   
+    
     private Map<String, Float> getHeuristicFromFile() {
         Map<String, Float> heuristic = new HashMap<String, Float>();
         Scanner file = new Scanner(path + heuristicFileName);
@@ -59,4 +86,50 @@ public class Tree {
         }
             
     }
+
+    private SortedSet<String> getDictionaryFromFile() {
+        SortedSet<String> dic = new TreeSet<String>();
+        Scanner file = new Scanner(path + dicFileName);
+        
+        while(file.hasNextLine()) {
+            String line = file.nextLine();
+            dic.add(line);
+        }
+        
+        file.close();
+        
+        if (dic.size() > 0) {
+            return dic;
+        }
+        else {
+            System.out.println("Dicionário vazio!");
+            return null;
+        }
+    }
+
+    private List<Node> sortedAdd(List<Node> frontier, List<Node> input, Map<String, Float> heuristic) {
+        
+        for(Node n: input) {
+            float function = n.getCost() + heuristic.get("$ "+n.getName()); //Evaluate function
+            n.setFunction(function);
+            int i = 0; boolean inseriu = false;
+            
+            if (!frontier.isEmpty()) {
+                while(i < frontier.size() && !inseriu) {
+                    Node aux = frontier.get(i);                    
+                    if(aux.getFunction() > function) {
+                        frontier.add(i, n);
+                    }
+                    else
+                        i++;
+                }
+            }
+            
+            if(!inseriu)
+                frontier.add(n);
+        }
+        
+        return frontier;
+    }
+    
 }
